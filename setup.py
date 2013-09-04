494dbd51fedc4b1fbfac36f974d7d64480d4fb27
Jan Češpivo <jan.cespivo@coex.cz>
2013-09-04 16:39:53 +0200

    code initialization

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+PACKAGE = "spamcheck"
+NAME = "spamcheck"
+DESCRIPTION = "Spam checker"
+AUTHOR = "Jan Češpivo, COEX CZ s.r.o (http://www.coex.cz)"
+AUTHOR_EMAIL = "info@coex.cz"
+URL = "https://github.com/COEXCZ/spamcheck"
+VERSION = '1.0.0'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="LICENSE.txt",
+    url=URL,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    install_requires=[
+        "simplejson",
+    ],
+)
---
+++ spamcheck/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.0.0'
+__all__ = ('spamcheck',)
+__author__ = "Jan Češpivo, COEX CZ s.r.o (http://www.coex.cz)"
+
+from urllib2 import Request, urlopen
+from simplejson import dumps, loads
+
+try:
+    from django.conf import settings
+    SPAM_CHECK_URL = getattr(settings, 'SPAM_CHECK_URL')
+except (ImportError, AttributeError):
+    SPAM_CHECK_URL = 'http://spamcheck.postmarkapp.com/filter'
+
+
+def spamcheck(raw_email, url=SPAM_CHECK_URL):
+
+    data = dict(email=raw_email, options='long')
+    json = dumps(data)
+
+    req = Request(url, json, {'Accept': 'application/json', 'Content-Type': 'application/json'})
+    result = urlopen(req)
+    for row in result:
+        response = loads(row)
+    result.close()
+
+    success = response['success']
+    spam_score = float(response['score'])
+    spam_report = response['report']
+
+    return success, spam_score, spam_report
